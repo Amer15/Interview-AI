b586fa433b72b2cd04f0bdc445987eb3e7f6b50b
Amer15 <amer95521@gmail.com>
2025-05-28 17:01:48 +0530

    setup: firebase SDK

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,48 @@
+import InterviewCard from "@/components/interview-card";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Excel in interviews with AI-powered mocks and feedbacks</h2>
+          <p className="text-lg">
+            Practice on real interview questions and get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="AI robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Homepage;
---
+++ firebase-config/client.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApp, getApps } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTwGtVIKurS9V5QuVD_jr5oEjfutX2lqc",
+  authDomain: "interview-ai-e4fc8.firebaseapp.com",
+  projectId: "interview-ai-e4fc8",
+  storageBucket: "interview-ai-e4fc8.firebasestorage.app",
+  messagingSenderId: "1057312643850",
+  appId: "1:1057312643850:web:b6c71f6815c76551f938f3",
+  measurementId: "G-WRBZRQKX0K",
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
